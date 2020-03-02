f1fc3c933cad1349a64a32c6bf4ce5f898313685
dheerajsatish <61401924+dheerajsatish@users.noreply.github.com>
2020-03-02 10:08:52 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int sum,difference,product,mod,divison,a,b;
+printf(" enter a \n");
+scanf("%d",&a);
+printf(" enter b \n");
+scanf("%d",&b);
+sum=a+b;
+difference=a-b;
+product=a*b;
+mod=a%b;
+divison=a/b;
+printf("sum= %d:", sum);
+printf("difference = %d", difference);
+printf("product= %d:",product);
+printf("mod %d", mod);
+printf("divison %d:", divison);
+}
