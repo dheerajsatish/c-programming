ef678d18e1393c9c9758493689ff0b5e0d1be2b7
dheerajsatish <61401924+dheerajsatish@users.noreply.github.com>
2020-03-09 08:53:10 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int a;
+printf("enter the number \n");
+scanf("%d", &a);
+if(a%2==0)
+{
+printf("%d is even \n", a);
+}
+else
+{
+printf("%d is odd \n", a);
+}
+}
---
+++ tempconversion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float fahrenheit, celcius;
+//get the limit of fibonacci series
+printf("enter fahrenheit: \n");
+scanf("%f",&fahrenheit);
+celcius=(fahrenheit-32)*5/9;
+printf("celcius: %f \n",celcius);
+}
